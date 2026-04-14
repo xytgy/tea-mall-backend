434634411285b397425a68fb8bc927ad2de62218
xytgy <xytgy99@qq.com>
2026-04-14 16:58:34 +0800

    feat:初始化提交

---
+++ src/main/java/com/xytgy/teamallbackend/TeaMallBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xytgy.teamallbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaMallBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaMallBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xytgy/teamallbackend/TeaMallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xytgy.teamallbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaMallBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
