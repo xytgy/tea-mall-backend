346ea1fa14282a77947103b7a726830073e514dc
xytgy <xytgy99@qq.com>
2026-04-25 13:29:29 +0800

    feat: 新增CategoryMapper接口用于MapStruct对象映射

---
+++ src/main/java/com/xytgy/teamallbackend/common/mapstruct/CopyMapper.java
@@ -0,0 +1,11 @@
+package com.xytgy.teamallbackend.common.mapstruct;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+// componentModel = "spring" 表示生成的实现类会标注 @Component，方便 Spring 注入
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+
+
+}
